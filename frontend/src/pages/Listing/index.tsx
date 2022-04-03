305e98359ed1bee1759e6e317f65a37021d4f83c
Francisco Salgado <franciscodsalgado@gmail.com>
2022-04-03 14:07:53 -0300

    Add moviecard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,31 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import './styles.css';
 
 function Listing() {
     return (
-       <Pagination />
+        < >
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
